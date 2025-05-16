50c7d9a3a994c3103b87a25ad2ecb79f74104097
charpitha-1 <thavishala279@gmail.com>
2025-05-16 17:56:36 +0530

    Add files via upload

---
+++ 15-05-2025/StreamIoDemo/StreamIoDemo/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StreamIoDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestStreamWriter();
+            //TestStreamWriterWithEncoding(); //first write and after that read\
+            //TestStreamReaderWithEncoding();
+            //TestFileStream_Write();
+            //TestFileStream_Read();
+            //TestBinaryWriter();
+            //TestBinaryReader();
+            //TestStreamReader();
+            ListFiles("C:\\Users\\thavishala.c");
+        }
+        private static void DoFileLoop()
+        {
+            //File.Copy();
+            //File.Delete();
+            //File.Move();
+            //File.Exists();
+        }
+        private static void ListFiles(string FolderPath)
+        {
+            if (Directory.Exists(FolderPath))
+            {
+                string[] files = Directory.GetFiles(FolderPath);
+                foreach (string file in files)
+                {
+                    Console.WriteLine("File  is about--->"+file);
+                    FileInfo fileInfo = new FileInfo(file);
+                    FileAttributes fileAttributes = fileInfo.Attributes;
+                    
+                }
+                string[] dirs = Directory.GetDirectories(FolderPath);
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+            }
+        }
+        private static void TestBinaryReader()
+        {
+            FileStream fs = new FileStream("file5.txt", FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long storedint = br.ReadInt64();//ReadInt32();
+            double storeddouble=br.ReadDouble();
+            Console.WriteLine(storedint);
+            Console.WriteLine(storeddouble);
+            br.Dispose();
+        }
+        
+        private static void TestBinaryWriter()
+        {
+            FileStream fs = new FileStream("file5.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs); 
+            bw.Write(1200L);
+            bw.Write(1233.34);
+            bw.Dispose();
+        }
+        private static void TestFileStream_Write()
+        {
+            FileStream fs = new FileStream("file4.txt",FileMode.OpenOrCreate,FileAccess.Write);
+            string strvalue = "My file stream is here";
+            byte[] barr=System.Text.Encoding.UTF8.GetBytes(strvalue);
+            Console.WriteLine(barr);
+            fs.Write(barr, 0, barr.Length);//bytes[22]
+            fs.Dispose();
+        }
+        private static void TestFileStream_Read()
+        {
+            FileStream ff = new FileStream("file4.txt", FileMode.Open, FileAccess.Read);
+            long len=ff.Length;
+            byte[] arrb=new byte[len];
+            ff.Read(arrb, 0, arrb.Length);
+            ff.Dispose();
+            String output=System.Text.Encoding.UTF8.GetString(arrb);
+            Console.WriteLine(output);
+        }
+        private static void TestStreamWriter()
+        {
+
+            string str = "My Test String2";
+            StreamWriter writer = new StreamWriter("File2.txt");
+            writer.WriteLine(str);
+            writer.Dispose();
+        }
+        private static void TestStreamReader()
+        {
+            StreamReader reader = new StreamReader("File3.txt");
+
+            //do
+            //{
+            //    string strout = reader.ReadLine();
+            //    if (strout == null)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(strout);
+            //} while (true);
+            string strtotal = reader.ReadToEnd();//reading entire content at a time
+            Console.WriteLine(strtotal);
+            reader.Dispose();
+        }
+        private static void TestStreamWriterWithEncoding()
+        {
+            string str1 = "My Test String to read ";
+            StreamWriter writer1 = new StreamWriter("File3.txt",false,Encoding.UTF32);
+            writer1.WriteLine(str1);
+            writer1.Dispose();
+        }
+        private static void TestStreamReaderWithEncoding()
+        {
+          
+            StreamReader reader1 = new StreamReader("File3.txt", Encoding.UTF32);
+            string strout1 = reader1.ReadLine();
+            Console.WriteLine(strout1);
+            reader1.Dispose();
+        }
+    }
+}
---
+++ 15-05-2025/StreamIoDemo/StreamIoDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StreamIoDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mphasis")]
+[assembly: AssemblyProduct("StreamIoDemo")]
+[assembly: AssemblyCopyright("Copyright © Mphasis 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7149daf-59f3-4330-ab74-6ab84a1ad01a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

bbb9c4df288431d9daaf657fc71c7810a6e13ad2
charpitha-1 <thavishala279@gmail.com>
2025-06-13 17:55:45 +0530

    Add files via upload

---
+++ 12-06-2025/Web2/Web2/AddComment.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Web2.Data;
+using Web2.DTO;
+
+namespace Web2
+{
+    public partial class AddComment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (DataAccess data = new DataAccess())
+                {
+                    List<BlogPost> posts = data.GetAllPosts();
+                    DrpPost.DataSource = posts;
+                    DrpPost.DataTextField = "Title";
+                    DrpPost.DataValueField = "PostId";
+                    DrpPost.DataBind();
+                }
+            }
+            
+
+        }
+    
+
+        protected void btnAddCom_Click(object sender, EventArgs e)
+        {
+            long PostId = Convert.ToInt64(DrpPost.SelectedItem.Value);
+            string CommentTitle=txtComtitle.Text;
+            string CommentText=txtComText.Text;
+            long CommentBy= Convert.ToInt64(Session["UserId"]);
+            //long PostId = long.Parse(Request.QueryString["pid"]);
+            
+            using (DataAccess data = new DataAccess())
+            {
+                bool res = data.AddBlogComment(PostId,CommentTitle, CommentText, CommentBy);
+                if(res)
+                {
+                    Response.Redirect("ViewComments.aspx");
+                }
+            }
+        }
+    }
+}
